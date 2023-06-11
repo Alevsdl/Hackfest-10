71d63df5b936cb9d9a29e4eb995a224e7557e625
Alevsdl <alejandravsdl@gmail.com>
2023-06-10 22:46:38 -0600

    paginas para agregar rutas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
-import Image from 'next/image'
-import bgg from '@/images/bg2.jpg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import Navbar from '@/components/navbar'
+import Image from 'next/image';
+import bgg from '@/images/bg2.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import Navbar from '@/components/navbar';
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-[white] overflow-x-hidden">
+    <div className="h-screen w-screen overflow-x-hidden bg-[white]">
       <Navbar />
-      <div className="grid grid-cols-1 sm:grid-cols-4 bg-[white] h-[100%] m-0 p-0 ">
+      <div className="m-0 grid h-[100%] grid-cols-1 bg-[white] p-0 sm:grid-cols-4 ">
         <div className="border-r border-[black]"></div>
-        <div className="border-r border-[black] sm:col-span-2 flex justify-center">
-          <Image src={bgg} alt="selfie" height={800} className="opacity-[40%] w-[100%]" />
-          <div className="text-container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center ">
-            <p className="text-[white] shadow-lg border-2 text-6xl font-semibold m-4 py-2 px-4 bg-black bg-opacity-20">¿A dónde quieres ir?</p>
-            <div className="flex justify-center items-center ">
+        <div className="flex justify-center border-r border-[black] sm:col-span-2">
+          <Image src={bgg} alt="selfie" height={800} className="w-[100%] opacity-[40%]" />
+          <div className="text-container absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center ">
+            <p className="m-4 border-2 bg-black bg-opacity-20 px-4 py-2 text-6xl font-semibold text-[white] shadow-lg">
+              ¿A dónde quieres ir?
+            </p>
+            <div className="flex items-center justify-center ">
               <div className="cursor-pointer text-8xl text-[#F24C3D] transition duration-500 hover:scale-125 hover:text-maingreen ">
                 <FontAwesomeIcon icon={faLocationDot} />
               </div>
@@ -24,6 +26,5 @@ export default function Home() {
         <div className="">3</div>
       </div>
     </div>
-  )
+  );
 }
-
---
+++ pages/ruta1.tsx
@@ -0,0 +1,14 @@
+
+import Navbar from '@/components/navbar';
+
+export default function Ruta1() {
+    return (
+        <div className="h-screen w-screen overflow-x-hidden bg-[white]">
+            <Navbar />
+            <div className="m-0 grid h-[100%] grid-cols-1 bg-[white] p-0 sm:grid-cols-4 ">
+
+                aqui van las rutas
+            </div>
+        </div>
+    );
+}
