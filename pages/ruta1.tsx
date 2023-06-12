bcd2ceba329e36b4a7476148b2736fdcf48abed7
VinRicci <vinicioricci@gmail.com>
2023-06-11 21:00:11 -0600

    Marcadores creados

--- pages/ruta1.tsx
+++ pages/ruta1.tsx
@@ -1,15 +1,13 @@
-
 import Navbar from '@/components/navbar';
-import  Ruta20  from '@/components/ruta20';
+import Ruta20 from '@/components/marcadores';
 
 export default function Ruta1() {
-    return (
-        <div className="h-screen w-screen overflow-x-hidden bg-[white]">
-            <Navbar />
-            <div className="m-0 grid h-[100%] grid-cols-1 bg-[white] p-0 sm:grid-cols-4 ">
-
-                <Ruta20 />
-            </div>
-        </div>
-    );
+  return (
+    <div className="h-screen w-screen overflow-x-hidden bg-[white]">
+      <Navbar />
+      <div className="m-0 grid h-[100%] grid-cols-1 bg-[white] p-0 sm:grid-cols-4 ">
+        <Ruta20 />
+      </div>
+    </div>
+  );
 }
