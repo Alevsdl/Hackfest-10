797d12aa3598e67eee690d00eb0abfaf9177f4af
VinRicci <vinicioricci@gmail.com>
2023-06-11 20:30:46 -0600

    Componente de rutas 19, A1 y 11 creado

--- pages/ruta1.tsx
+++ pages/ruta1.tsx
@@ -1,15 +1,13 @@
-
 import Navbar from '@/components/navbar';
-import  Ruta20  from '@/components/ruta20';
+import RutaA1 from '@/components/ruta19';
 
 export default function Ruta1() {
-    return (
-        <div className="h-screen w-screen overflow-x-hidden bg-[white]">
-            <Navbar />
-            <div className="m-0 grid h-[100%] grid-cols-1 bg-[white] p-0 sm:grid-cols-4 ">
-
-                <Ruta20 />
-            </div>
-        </div>
-    );
+  return (
+    <div className="h-screen w-screen overflow-x-hidden bg-[white]">
+      <Navbar />
+      <div className="m-0 grid h-[100%] grid-cols-1 bg-[white] p-0 sm:grid-cols-4 ">
+        <RutaA1 />
+      </div>
+    </div>
+  );
 }
