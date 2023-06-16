bdc9136e8248b070c08fed94e403e09e6587b8ca
Alejandra Santos <49631442+Alevsdl@users.noreply.github.com>
2023-06-16 12:30:07 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <Navbar />
       {isMobile ? (
         <div className="m-0 grid h-[100%] grid-cols-1 bg-[white] p-0 sm:grid-cols-4 ">
-          <div className="flex justify-center border-r border-[black] sm:col-span-2 z-50">
+          <div className="flex justify-center border-r border-[black] sm:col-span-2 ">
             <Image src={bgg} alt="selfie" height={800} className="w-[100%] opacity-[40%]" />
             <div className="text-container absolute left-1/2 top-[42%] -translate-x-1/2 -translate-y-1/2 transform text-center ">
               <p className="m-4 border-2 bg-black bg-opacity-20 px-4 py-2 text-5xl font-semibold text-[white] shadow-lg">
@@ -43,7 +43,7 @@ export default function Home() {
             </div>
           </div>
           <div className="border-yellow-500 border-t-8 pr-4 pl-4">
-            <div className="pt-4 z-20">
+            <div className="pt-4">
               <div className="group relative inline-flex items-center justify-start sm:overflow-hidden rounded-xl bg-yellow-100 px-6 py-3 font-medium transition-all">
                 <span className="absolute right-0 top-0 inline-block h-4 w-4 rounded bg-yellow-700 transition-all duration-500 ease-in-out group-hover:-mr-4 group-hover:-mt-4">
                   <span className="absolute right-0 top-0 h-5 w-5 -translate-y-1/2 translate-x-1/2 rotate-45 bg-white"></span>
